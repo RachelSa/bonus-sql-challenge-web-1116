814193c88745725d864df19eea122af4d743e31f
github username <github email address>
2016-12-11 20:50:23 -0500

    almost done

--- answers.sql
+++ answers.sql
@@ -1,1 +1,59 @@
 -- Write your queries here!
+Who did Jon Stewart have on the Daily Show the most?
+Fareed Zakaria
+
+What was the most popular profession of guest for each year Jon Stewart hosted the Daily Show?
+1999
+pianist
+2000
+Singer-lyricis
+2001
+Guitarist
+former mayor of cincinatti
+muppet
+2002
+Model
+2003
+model
+2004
+television presenter
+2005
+Commentator
+former governor of nebraska
+2006
+Former Assistant to the President for Communications
+former governor of new jersey
+2007
+former american senator
+2008
+former governor of vermont
+2009
+american politician
+2010
+rapper
+2011
+commentator
+2012
+editor
+2013
+NA
+2014
+writer
+2015
+actor
+
+What profession was on the show most overall?
+actor
+
+How many people did Jon Stewart have on with the first name of Bill?
+
+What dates did Patrick Stewart appear on the show?
+
+4/12/00
+4/21/03
+11/7/13
+
+Which year had the most guests?
+2000
+
+What was the most popular "Group" for each year Jon Stewart hosted?
--- parse.rb
+++ parse.rb
@@ -1,1 +1,34 @@
 # parse the CSV into a sqlite3 database here!
+
+#YEAR,GoogleKnowlege_Occupation,Show,Group,Raw_Guest_List
+
+require "sqlite3"
+require 'csv'
+#require_relative '/daily_show_guests.csv'
+
+DB = {:conn => SQLite3::Database.new("guests.db")}
+DB[:conn].execute("DROP TABLE IF EXISTS guests")
+
+sql = <<-SQL
+  CREATE TABLE IF NOT EXISTS guests (
+    Year INTEGER,
+    Occupation TEXT,
+    Show TEXT,
+    Type TEXT,
+    Name TEXT
+  );
+SQL
+
+DB[:conn].execute(sql)
+
+CSV.foreach("daily_show_guests.csv") do |row|
+  sql = "INSERT INTO guests (Year, Occupation, Show, Type, Name) 
+    VALUES (?, ?, ?, ?, ?);"
+
+  DB[:conn].execute(sql, row[0], row[1], row[2], row[3], row[4])
+end
+
+
+ full = DB[:conn].execute( "SELECT year, type, COUNT(type) FROM guests 
+GROUP BY type ORDER BY year DESC, COUNT(type) DESC" ) 
+ puts full
